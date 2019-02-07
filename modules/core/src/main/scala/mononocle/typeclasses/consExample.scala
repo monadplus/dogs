915d85fe92ab4913ed905994cb604b98d4c13719
statictypeddog <arnauabella@gmail.com>
2019-02-07 18:32:23 +0100

    Lot of resources in monocle to explore :) but I'll give up for now until I need an optic.

--- modules/core/src/main/scala/mononocle/typeclasses/consExample.scala
+++ modules/core/src/main/scala/mononocle/typeclasses/consExample.scala
@@ -1,3 +1,26 @@
 package mononocle.typeclasses
 
-object consExample {}
+import monocle.Lens
+import monocle.function.At
+
+/**
+ * Typeclass that defines a [[monocle.Prism]] between an `S` and its head `A` and tail `S`
+ * @tparam S source of [[monocle.Prism]] and tail of [[monocle.Prism]] target
+ * @tparam A head of [[monocle.Prism]] target, `A` is supposed to be unique for a given `S`
+ */
+object consExample {
+  type DayOfWeek = String
+  case class Cooker(name: String)
+  case class Restaurant(cookers: Map[DayOfWeek, Cooker])
+  val restaurant = Restaurant(
+    Map(
+      "Monday"  -> Cooker("John"),
+      "Tuesday" -> Cooker("Antonio")
+    )
+  )
+  val cookerAt = At[Restaurant, DayOfWeek, Cooker](d => office => office.cookers(d))(
+    d => e => o => o.copy(cookers = o.cookers ++ Map(d -> e))
+  )
+  val setCookerMonday: Lens[Restaurant, Cooker] = cookerAt.at("Monday")
+  println(setCookerMonday.set(Cooker("Jose"))(restaurant))
+}
--- modules/core/src/main/scala/mononocle/typeclasses/eachExample.scala
+++ modules/core/src/main/scala/mononocle/typeclasses/eachExample.scala
@@ -1,3 +1,23 @@
 package mononocle.typeclasses
 
-object eachExample {}
+import monocle.Iso
+
+object eachExample extends App {
+  case class Moon(name: String) extends AnyVal
+  case class Planet(name: String, moons: Vector[Moon])
+
+  val planet = Planet(
+    name = "Saturn",
+    moons = Vector(Moon("Tethys"), Moon("Dione"))
+  )
+
+  import monocle.Lens
+  import monocle.macros.GenLens
+  import monocle.macros.GenIso
+  import monocle.function.Each._ // implicits ! (have a look)
+
+  val satellites: Lens[Planet, Vector[Moon]] = GenLens[Planet](_.moons)
+  val satelliteName: Iso[Moon, String]       = GenIso[Moon, String]
+  val planetNames                            = satellites.composeTraversal(each).composeIso(satelliteName)
+  println(planetNames.getAll(planet))
+}
